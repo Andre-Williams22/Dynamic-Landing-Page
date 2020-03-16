f508c3c2c828e4801bc42089e7d6c55fc8716c7d
Andre Williams <awjrs22@gmail.com>
2020-03-16 16:54:36 -0700

    cleans up front end and starts submit route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -9,6 +9,16 @@ def index():
 
 
 
+@app.route('/submit', methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        first = request.form['first']
+        last = request.form['last']
+        age = request.form['age']
+        gender = request.form['gender']
+        email = request.form['email']
+        number = request.form['number']
 
 if __name__ == '__main__':
+    app.debug = True
     app.run(debug=True)
